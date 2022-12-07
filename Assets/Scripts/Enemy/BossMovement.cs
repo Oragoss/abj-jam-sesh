61f4d305458e093f07ae11607c94a3d63527ee51
Jon Sousa <jonathonsousa@gmail.com>
2022-12-07 16:01:54 -0600

    Removed committed commented out code

--- Assets/Scripts/Enemy/BossMovement.cs
+++ Assets/Scripts/Enemy/BossMovement.cs
@@ -27,38 +27,10 @@ namespace Assets.Scripts.Enemy
         {
             int randomRange = Random.Range(0, movePoints.Count);
             currentTarget = movePoints[randomRange];
-
-            //Lazy way to just keep repeating until it's not the same?
-            //while(randomRange == lastPositionUsed)
-            //    randomRange = Random.Range(0, movePointOptions.Count);
-
-
-            //TODO: Make it not use the same
-            //movePointsAlreadyUsed.Add(movePointOptions[randomRange]);
-            //movePoints.Clear();
-
-            //if (movePointOptions.Count != movePointsAlreadyUsed.Count)
-            //{
-            //    for (int x = 0; x < movePointOptions.Count; x++)
-            //    {
-            //        for (int y = 0; x < movePointsAlreadyUsed.Count; y++)
-            //        {
-            //            if (movePointOptions[x] != movePointsAlreadyUsed[y])
-            //            {
-            //                movePoints.Add(movePointOptions[x]);
-            //            }
-            //        }
-            //    }
-
-            //}
-            //else {
-            //    movePointsAlreadyUsed.Clear();
-            //}
         }
         private void Movement()
         {
             rigidbody.position = Vector2.MoveTowards(transform.position, currentTarget.position, speed * 50 * Time.deltaTime);
-            //transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
         }
 
         private void OnDestroy()
